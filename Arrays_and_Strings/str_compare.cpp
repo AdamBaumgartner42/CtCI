f3157512e269c7e9579d4e6ef32c726ca2c02139
adambaumgartner42 <adambaumgartner@gmail.com>
2026-01-02 20:52:50 -0500

    initial commit

---
+++ Arrays_and_Strings/str_compare.cpp
@@ -0,0 +1,21 @@
+/**
+ * @brief Functions to work on strings
+ * @author Adam Baumgartner
+ * @date 1/2/2026
+ * @note g++ hello.cpp -o hello_app
+ */
+
+
+
+#include <iostream>
+#include <string>
+
+
+bool isPerm(){
+    return true;
+}
+
+int main (int argc, char* argv[]){
+
+}
+
